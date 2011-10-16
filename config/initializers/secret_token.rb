0d4edceda16fa9a0068958d41ad6641a55e31007
Amir Barylko <amir@barylko.com>
2011-10-16 13:31:07 -0500

    First commit after generating the base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SdecDemo::Application.config.secret_token = '8d9d76e6261e9f85214b298afd2111c7cb9441866f1daf871962007befe327773c62a45d1936cbf7c0cce1241a1f7e0bd1b1298a734f54babb140f1f535a5ab9'
