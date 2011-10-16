9fa92e691883150ed5d59064b0f909f209674c63
Amir Barylko <amir@barylko.com>
2011-10-16 18:38:54 -0500

    Added route to / and removed index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SdecDemo::Application.routes.draw do
   resources :movies
 
-  #root :to => 'movies#index'
+  root :to => 'movies#index'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
